eaff8e87ebcc767ced8e67d2e1e8d279f9e7dc3e
Deibeat9 <davidquintero9@hotmail.com>
2026-06-16 11:14:44 -0600

    P12R01

---
+++ P12R01_Calculadora_MVC_CDQV/Models/CalculadoraModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P12R01_Calculadora_MVC_CDQV.Models
+{
+    public class CalculadoraModel
+    {
+        [Required(ErrorMessage ="El primer numero es requerido")]
+        [Display(Name = "Primer Numero")]
+
+        public double Numero1 { get; set; }
+        public double Numero2 { get; set; }
+        public string Operacion { get; set; }
+        public double? Resultado { get; set; }
+
+
+        public void Calcular()
+        {
+            switch (Operacion)
+            {
+                case "sumar":
+                    Resultado = Numero1 + Numero2; 
+                    break;
+                case "restar":
+                    Resultado = Numero1 - Numero2; 
+                    break;
+                case "multiplicar":
+                    Resultado = Numero1 * Numero2; 
+                    break;
+                case "dividir":
+                    if(Numero2 != 0)
+                        Resultado = Numero1 / Numero2;
+                    else 
+                        Resultado = null;
+                    break;
+                default:
+                    Resultado = null;
+                    break;
+
+            }
+        }
+
+
+    }
+}
---
+++ P12R01_Calculadora_MVC_CDQV/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P12R01_Calculadora_MVC_CDQV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
